a5dbe04cb7ab0114cd34d6a185dd0cb9df69fdd7
johangenis <johan.j.genis@gmail.com>
2020-06-02 13:01:40 +0200

    Added logic to authenticate user.

--- UserSignUpView/starter_code_5/src/core/views.py
+++ UserSignUpView/starter_code_5/src/core/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .forms import UserRegisterForm
 
 
 def register_view(request):
-    form = UserRegisterForm()
+    form = UserRegisterForm(request.POST)
     if request.method == "POST":
-        form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
+            password = request.POST.get("password")
+            user.set_password(password)
+            user.save()
+            auth_user = authenticate(username=user.username, password=password)
+            login(request, auth_user)
+            return redirect("/")
+
     context = {"form": form}
     return render(request, "register.html", context)

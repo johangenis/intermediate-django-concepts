ff7f02b73c191b2a8a38c6ee6b7c1ff2341178b8
johangenis <johan.j.genis@gmail.com>
2020-06-02 12:47:40 +0200

    Initial commit after downloading and running SECTION 2. VIEWS User signup view.

--- UserSignUpView/starter_code_5/src/core/views.py
+++ UserSignUpView/starter_code_5/src/core/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 
 from .forms import UserRegisterForm
 
+
 def register_view(request):
     form = UserRegisterForm()
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-    context = {
-        'form': form
-    }
+            user = form.save(commit=False)
+    context = {"form": form}
     return render(request, "register.html", context)

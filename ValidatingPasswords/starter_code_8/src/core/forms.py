3e5b06d4d8692f15237cb6d14235856df5e26f5b
johangenis <johan.j.genis@gmail.com>
2020-06-02 18:31:54 +0200

    SECTION 2. VIEWS Validating Passwords.

--- ValidatingPasswords/starter_code_8/src/core/forms.py
+++ ValidatingPasswords/starter_code_8/src/core/forms.py
@@ -5,27 +5,31 @@ from django import forms
 # settings.AUTH_USER_MODEL
 User = get_user_model()
 
+
 class UserRegisterForm(forms.ModelForm):
+    password2 = forms.CharField()
+
     class Meta:
         model = User
-        fields = [
-            'username', 'email', 'password'
-        ]
+        fields = ["username", "email", "password", "password2"]
+
 
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
 
     def clean(self, *args, **kwargs):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
 
         if username and password:
             user = authenticate(username=username, password=password)
             if not user:
-                raise forms.ValidationError('This user does not exist')
+                raise forms.ValidationError("This user does not exist")
             if not user.check_password(password):
-                raise forms.ValidationError('The username or password was incorrect')
+                raise forms.ValidationError(
+                    "The username or password was incorrect"
+                )
             if not user.is_active:
-                raise forms.ValidationError('This user is not active')
+                raise forms.ValidationError("This user is not active")
         return super(UserLoginForm, self).clean(*args, **kwargs)
